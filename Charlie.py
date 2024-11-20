3ee2fcce4f86f1f076e4305c39403c49ef887f0a
chajardi <cjardine001@student.franciscan.edu>
2024-11-20 18:39:56 -0500

    organizing form

--- Charlie.py
+++ Charlie.py
@@ -3,36 +3,308 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Window")
 
-page_1 = tk.Frame(
+page_4 = tk.Frame(
     master=window, 
     width=750, 
     height=475, 
     bg="light grey"
 )
-Question_1 = tk.Label(                  #Measuring Opennes
-    master=page_1,
-    text= "Question TEXT GOES HERE",
+question_labels_4 = tk.Frame(
+    master=page_4,
+    bg="light grey",
+    width=275, 
+    height=475,
+)
+answers_labels_4 = tk.Frame(
+    master=page_4,
+    bg="light grey",
+    width=500, 
+    height=510,
+)
+
+def next4to5():
+    page_4.forget()
+    page_5.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+def next5tofinish():
+    page_5.forget()
+    #PACK the final page here 
+
+
+page_5 = tk.Frame(
+    master=window, 
+    width=750, 
+    height=475, 
+    bg="light grey"
+)
+question_labels_5 = tk.Frame(
+    master=page_5,
+    bg="light grey",
+     width=275, 
+    height=475
+)
+answers_labels_5 = tk.Frame(
+    master=page_5,
+    bg="light grey",
+     width=500, 
+    height=510,
+)
+
+
+Question_13 = tk.Label(                  #Measuring Openness
+    master=question_labels_4,
+    text= "How do you react when meeting new people?",
     bg= "light grey"
 )
-q1_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
-    text= "ANSWER TEXT GOES HERE",
+q13_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master= answers_labels_4,
+    text= "I don't find myself excited, but I am not scared to.",
     relief= "raised",
     bg= "grey"
 )
-q1_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
-    text= "ANSWER TEXT GOES HERE",
+q13_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I love to meet new people, and strike up conversation with them.",
     relief= "raised",
     bg= "grey"
 )
-q1_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
-    text= "ANSWER TEXT GOES HERE",
+q13_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I get nervous and tend to avoid meeting new people.",
     relief= "raised",
     bg= "grey"
 )
 
-page_1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-Question_1.place(x= 0, y= 0)
-q1_a1.place(x=200, y=0)
-q1_a2.place(x=200, y=30)
-q1_a3.place(x=200, y=60)
+Question_14 = tk.Label(                  #Measuring Agreableness
+    master=question_labels_4,
+    text= "How do you typically treat others?",
+    bg= "light grey"
+)
+q14_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_4,
+    text= "I don't think I am particularly kind, but I don't think I am mean.",
+    relief= "raised",
+    bg= "grey"
+)
+q14_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I try to always treat others with kindness and respect.",
+    relief= "raised",
+    bg= "grey"
+)
+q14_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I am typically not sensitive of other peoples feelings.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_15 = tk.Label(                  #Measuring Neuroticism
+    master=question_labels_4,
+    text= "How Often do you feel sad or depressed?",
+    bg= "light grey"
+)
+q15_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_4,
+    text= "I only find myself feeling sad or depressed when a situation warrants it.",
+    relief= "raised",
+    bg= "grey"
+)
+q15_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I dont find myself feeling sad or depressed.",
+    relief= "raised",
+    bg= "grey"
+)
+q15_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I frequently feel sad or depressed.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_16 = tk.Label(                  #Measuring Consientiousness
+    master=question_labels_4,
+    text= "What's your process when making decisions?",
+    bg= "light grey"
+)
+q16_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_4,
+    text= "Sometimes I plan things out, but I also trust my gut.",
+    relief= "raised",
+    bg= "grey"
+)
+q16_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I think about the consequences of one decision over another and choose the best one.",
+    relief= "raised",
+    bg= "grey"
+)
+q16_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I just trust my gut and go with what I feel..",
+    relief= "raised",
+    bg= "grey"
+)
+Next_p4 = tk.Button(
+    master=answers_labels_4,
+    text="Next",
+    relief="raised",
+    command=next4to5
+)
+
+
+Question_17 = tk.Label(                  #Measuring Neuroticism
+    master=question_labels_5,
+    text= "Do you get stressed easily?",
+    bg= "light grey"
+)
+q17_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "Yes, but sometimes I am better at managing it than other times.",
+    relief= "raised",
+    bg= "grey"
+)
+q17_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "No, but even when I do feel stressed, I bounce back quickly.",
+    relief= "raised",
+    bg= "grey"
+)
+q17_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "Yes, and once I'm stressed, it's hard for me to calm down.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_18 = tk.Label(                  #Measuring Extraversion
+    master=question_labels_5,
+    text= "How do you feel about small talk?",
+    bg= "light grey"
+)
+q18_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "It depends on how I am feeling, but occasionally I engage in small talk.",
+    relief= "raised",
+    bg= "grey"
+)
+q18_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "I consider myself a small talk expert.",
+    relief= "raised",
+    bg= "grey"
+)
+q18_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "Small talk makes me anxious, and I tend to avoid talking about myself.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_19 = tk.Label(                  #Measuring ???Not sure???
+    master=question_labels_5,
+    text= "Would your firends describe you as dependable?",
+    bg= "light grey"
+)
+q19_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "It depends on what we're doing, but generally yes.",
+    relief= "raised",
+    bg= "grey"
+)
+q19_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "Yes, I'm reliable and honor my commitments or plans.",
+    relief= "raised",
+    bg= "grey"
+)
+q19_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "Probably not. I tend to be kind of flaky.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_20 = tk.Label(                  #Measuring ???Not sure???
+    master=question_labels_5,
+    text= "How do you feel about repetitive tasks?",
+    bg= "light grey"
+)
+q20_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "Sometimes I need tasks like that to help me calm down.",
+    relief= "raised",
+    bg= "grey"
+)
+q20_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "I'm comfortable with repetitive tasks; routine is good.",
+    relief= "raised",
+    bg= "grey"
+)
+q20_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "I get bored easily and need variety whenever possible.",
+    relief= "raised",
+    bg= "grey"
+)
+Next_p5 = tk.Button(
+    master=answers_labels_4,
+    text="Next",
+    relief="raised",
+    command=next5tofinish
+)
+
+
+
+page_4.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+question_labels_4.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_4.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+Question_13.place(x= 0, y= 0)
+q13_a1.place(x=00, y=0)
+q13_a2.place(x=00, y=30)
+q13_a3.place(x=00, y=60)
+
+Question_14.place(x= 0, y= 120)
+q14_a1.place(x=00, y=120)
+q14_a2.place(x=00, y=150)
+q14_a3.place(x=00, y=180)
+
+Question_15.place(x= 0, y= 240)
+q15_a1.place(x=00, y=240)
+q15_a2.place(x=00, y=270)
+q15_a3.place(x=00, y=300)
+
+Question_16.place(x= 0, y= 360)
+q16_a1.place(x=00, y=360)
+q16_a2.place(x=00, y=390)
+q16_a3.place(x=00, y=420)
+
+Next_p4.place(x=0, y= 480)
+
+question_labels_5.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_5.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+Question_17.place(x= 0, y= 0)
+q17_a1.place(x=0, y=0)
+q17_a2.place(x=0, y=30)
+q17_a3.place(x=0, y=60)
+Question_18.place(x= 0, y= 120)
+q18_a1.place(x=0, y=120)
+q18_a2.place(x=0, y=150)
+q18_a3.place(x=0, y=180)
+Question_19.place(x= 0, y= 240)
+q19_a1.place(x=0, y=240)
+q19_a2.place(x=0, y=270)
+q19_a3.place(x=0, y=300)
+Question_20.place(x= 0, y= 360)
+q20_a1.place(x=0, y=360)
+q20_a2.place(x=0, y=390)
+q20_a3.place(x=0, y=420)
+
+Next_p5.place(x=0, y=480)
+
 window.mainloop()
--- Welcome Page.py
+++ Welcome Page.py
@@ -3,18 +3,517 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Window")
 
+
+###################################################################################
+
+#EVERYTHING TO DO WITH THE WELCOME PAGE
+
 page_welcome = tk.Frame(
     master=window, 
     width=750, 
     height=475, 
     bg="light grey"
 )
+
+
+
 welcome = tk.Label(
     master=page_welcome,
     text= "Welcome! \n Please press the button below to begin your Personality Quiz Journey:",
     bg= "light grey"
 )
+def start_to_p1():
+    page_welcome.forget()
+    page_2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+start = tk.Button(
+    master=page_welcome,
+    text="Begin",
+    command=start_to_p1
+)
 
 page_welcome.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 welcome.place(x= 200, y= 200)
+start.place(x=275, y=260)
+
+#####################################################################################
+
+#EVERYTHING TO DO WITH PAGE 1 OF QUESTIONS
+
+page_1 = tk.Frame(
+    master=window, 
+    width=750, 
+    height=475, 
+    bg="light grey"
+)
+question_labels_1 = tk.Frame(
+    master=page_1,
+    bg="light grey",
+    width=275, 
+    height=475,
+)
+answers_labels_1 = tk.Frame(
+    master=page_1,
+    bg="light grey",
+    width=500, 
+    height=510,
+)
+
+def one_to_two():
+    page_1.forget()
+    page_2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+Next_p1 = tk.Button(
+    master=page_1,
+    text="Next",
+    command=one_to_two
+)
+
+question_labels_1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+# Question_1.place(x= 0, y= 0)
+# q1_a1.place(x=00, y=0)
+# q1_a2.place(x=00, y=30)
+# q1_a3.place(x=00, y=60)
+
+# Question_2.place(x= 0, y= 120)
+# q2_a1.place(x=00, y=120)
+# q2_a2.place(x=00, y=150)
+# q2_a3.place(x=00, y=180)
+
+# Question_3.place(x= 0, y= 240)
+# q3_a1.place(x=00, y=240)
+# q3_a2.place(x=00, y=270)
+# q3_a3.place(x=00, y=300)
+
+# Question_4.place(x= 0, y= 360)
+# q4_a1.place(x=00, y=360)
+# q4_a2.place(x=00, y=390)
+# q4_a3.place(x=00, y=420)
+
+Next_p1.place(x=0, y= 480)
+
+##########################################################################################################
+
+#EVERYTHING TO DO WITH PAGE 2 OF QUESTIONS
+
+page_2 = tk.Frame(
+    master=window, 
+    width=750, 
+    height=475, 
+    bg="light grey"
+)
+question_labels_2 = tk.Frame(
+    master=page_2,
+    bg="light grey",
+    width=275, 
+    height=475,
+)
+answers_labels_2 = tk.Frame(
+    master=page_2,
+    bg="light grey",
+    width=500, 
+    height=510,
+)
+def two_to_three():
+    page_2.forget()
+    page_3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+Next_p2 = tk.Button(
+    master=page_2,
+    text="Next",
+    command=two_to_three
+)
+
+question_labels_2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+# Question_5.place(x= 0, y= 0)
+# q5_a1.place(x=00, y=0)
+# q5_a2.place(x=00, y=30)
+# q5_a3.place(x=00, y=60)
+
+# Question_6.place(x= 0, y= 120)
+# q6_a1.place(x=00, y=120)
+# q6_a2.place(x=00, y=150)
+# q6_a3.place(x=00, y=180)
+
+# Question_7.place(x= 0, y= 240)
+# q7_a1.place(x=00, y=240)
+# q7_a2.place(x=00, y=270)
+# q7_a3.place(x=00, y=300)
+
+# Question_8.place(x= 0, y= 360)
+# q8_a1.place(x=00, y=360)
+# q8_a2.place(x=00, y=390)
+# q8_a3.place(x=00, y=420)
+
+Next_p2.place(x=0, y= 480)
+
+###################################################################################################################
+
+#EVERYTHING TO DO WITH PAGE 3 OF QUESTIONS
+
+page_3 = tk.Frame(
+    master=window, 
+    width=750, 
+    height=475, 
+    bg="light grey"
+)
+question_labels_3 = tk.Frame(
+    master=page_3,
+    bg="light grey",
+    width=275, 
+    height=475,
+)
+answers_labels_3 = tk.Frame(
+    master=page_3,
+    bg="light grey",
+    width=500, 
+    height=510,
+)
+def three_to_four():
+    page_3.forget()
+    page_4.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+
+Next_p3 = tk.Button(
+    master=page_3,
+    text="Next",
+    command=three_to_four
+)
+
+question_labels_3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+# Question_9.place(x= 0, y= 0)
+# q9_a1.place(x=00, y=0)
+# q9_a2.place(x=00, y=30)
+# q9_a3.place(x=00, y=60)
+
+# Question_10.place(x= 0, y= 120)
+# q10_a1.place(x=00, y=120)
+# q10_a2.place(x=00, y=150)
+# q10_a3.place(x=00, y=180)
+
+# Question_11.place(x= 0, y= 240)
+# q11_a1.place(x=00, y=240)
+# q11_a2.place(x=00, y=270)
+# q11_a3.place(x=00, y=300)
+
+# Question_12.place(x= 0, y= 360)
+# q12_a1.place(x=00, y=360)
+# q12_a2.place(x=00, y=390)
+# q12_a3.place(x=00, y=420)
+
+Next_p3.place(x=0, y= 480)
+
+#################################################################################################################
+
+#EVERYTHING TO DO WITH PAGE 4 OF QUESTIONS
+
+page_4 = tk.Frame(
+    master=window, 
+    width=750, 
+    height=475, 
+    bg="light grey"
+)
+question_labels_4 = tk.Frame(
+    master=page_4,
+    bg="light grey",
+    width=275, 
+    height=475,
+)
+answers_labels_4 = tk.Frame(
+    master=page_4,
+    bg="light grey",
+    width=500, 
+    height=510,
+)
+
+def four_to_five():
+    page_4.forget()
+
+Next_p4 = tk.Button(
+    master=answers_labels_4,
+    text="Next",
+    relief="raised",
+    command=four_to_five
+)
+
+Question_13 = tk.Label(                  #Measuring Openness
+    master=question_labels_4,
+    text= "How do you react when meeting new people?",
+    bg= "light grey"
+)
+q13_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master= answers_labels_4,
+    text= "I don't find myself excited, but I am not scared to.",
+    relief= "raised",
+    bg= "grey"
+)
+q13_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I love to meet new people, and strike up conversation with them.",
+    relief= "raised",
+    bg= "grey"
+)
+q13_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I get nervous and tend to avoid meeting new people.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_14 = tk.Label(                  #Measuring Agreableness
+    master=question_labels_4,
+    text= "How do you typically treat others?",
+    bg= "light grey"
+)
+q14_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_4,
+    text= "I don't think I am particularly kind, but I don't think I am mean.",
+    relief= "raised",
+    bg= "grey"
+)
+q14_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I try to always treat others with kindness and respect.",
+    relief= "raised",
+    bg= "grey"
+)
+q14_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I am typically not sensitive of other peoples feelings.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_15 = tk.Label(                  #Measuring Neuroticism
+    master=question_labels_4,
+    text= "How Often do you feel sad or depressed?",
+    bg= "light grey"
+)
+q15_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_4,
+    text= "I only find myself feeling sad or depressed when a situation warrants it.",
+    relief= "raised",
+    bg= "grey"
+)
+q15_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I dont find myself feeling sad or depressed.",
+    relief= "raised",
+    bg= "grey"
+)
+q15_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I frequently feel sad or depressed.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_16 = tk.Label(                  #Measuring Consientiousness
+    master=question_labels_4,
+    text= "What's your process when making decisions?",
+    bg= "light grey"
+)
+q16_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_4,
+    text= "Sometimes I plan things out, but I also trust my gut.",
+    relief= "raised",
+    bg= "grey"
+)
+q16_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_4,
+    text= "I think about the consequences of one decision over another and choose the best one.",
+    relief= "raised",
+    bg= "grey"
+)
+q16_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_4,
+    text= "I just trust my gut and go with what I feel..",
+    relief= "raised",
+    bg= "grey"
+)
+
+question_labels_4.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_4.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+Question_13.place(x= 0, y= 0)
+q13_a1.place(x=00, y=0)
+q13_a2.place(x=00, y=30)
+q13_a3.place(x=00, y=60)
+
+Question_14.place(x= 0, y= 120)
+q14_a1.place(x=00, y=120)
+q14_a2.place(x=00, y=150)
+q14_a3.place(x=00, y=180)
+
+Question_15.place(x= 0, y= 240)
+q15_a1.place(x=00, y=240)
+q15_a2.place(x=00, y=270)
+q15_a3.place(x=00, y=300)
+
+Question_16.place(x= 0, y= 360)
+q16_a1.place(x=00, y=360)
+q16_a2.place(x=00, y=390)
+q16_a3.place(x=00, y=420)
+
+Next_p4.place(x=0, y= 480)
+
+##################################################################################################################
+
+#EVERYTHING TO DO WITH PAGE 5 OF QUESTIONS
+
+page_5 = tk.Frame(
+    master=window, 
+    width=750, 
+    height=475, 
+    bg="light grey"
+)
+question_labels_5 = tk.Frame(
+    master=page_5,
+    bg="light grey",
+     width=275, 
+    height=475
+)
+answers_labels_5 = tk.Frame(
+    master=page_5,
+    bg="light grey",
+     width=500, 
+    height=510,
+)
+def five_to_finish():
+    page_5.forget()
+    #PACK the final page here 
+
+Question_17 = tk.Label(                  #Measuring Neuroticism
+    master=question_labels_5,
+    text= "Do you get stressed easily?",
+    bg= "light grey"
+)
+q17_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "Yes, but sometimes I am better at managing it than other times.",
+    relief= "raised",
+    bg= "grey"
+)
+q17_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "No, but even when I do feel stressed, I bounce back quickly.",
+    relief= "raised",
+    bg= "grey"
+)
+q17_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "Yes, and once I'm stressed, it's hard for me to calm down.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_18 = tk.Label(                  #Measuring Extraversion
+    master=question_labels_5,
+    text= "How do you feel about small talk?",
+    bg= "light grey"
+)
+q18_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "It depends on how I am feeling, but occasionally I engage in small talk.",
+    relief= "raised",
+    bg= "grey"
+)
+q18_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "I consider myself a small talk expert.",
+    relief= "raised",
+    bg= "grey"
+)
+q18_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "Small talk makes me anxious, and I tend to avoid talking about myself.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_19 = tk.Label(                  #Measuring ???Not sure???
+    master=question_labels_5,
+    text= "Would your firends describe you as dependable?",
+    bg= "light grey"
+)
+q19_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "It depends on what we're doing, but generally yes.",
+    relief= "raised",
+    bg= "grey"
+)
+q19_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "Yes, I'm reliable and honor my commitments or plans.",
+    relief= "raised",
+    bg= "grey"
+)
+q19_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "Probably not. I tend to be kind of flaky.",
+    relief= "raised",
+    bg= "grey"
+)
+
+Question_20 = tk.Label(                  #Measuring ???Not sure???
+    master=question_labels_5,
+    text= "How do you feel about repetitive tasks?",
+    bg= "light grey"
+)
+q20_a1 = tk.Button(                      #Assigning a neutral value of 0 to this answer
+    master=answers_labels_5,
+    text= "Sometimes I need tasks like that to help me calm down.",
+    relief= "raised",
+    bg= "grey"
+)
+q20_a2 = tk.Button(                      #Assigning a positive value of postive 1 to this answer
+    master=answers_labels_5,
+    text= "I'm comfortable with repetitive tasks; routine is good.",
+    relief= "raised",
+    bg= "grey"
+)
+q20_a3 = tk.Button(                      #Assigning a negative value of -1 to this answer
+    master=answers_labels_5,
+    text= "I get bored easily and need variety whenever possible.",
+    relief= "raised",
+    bg= "grey"
+)
+Next_p5 = tk.Button(
+    master=answers_labels_4,
+    text="Next",
+    relief="raised",
+    command=five_to_finish
+)
+
+question_labels_5.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+answers_labels_5.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+Question_17.place(x= 0, y= 0)
+q17_a1.place(x=0, y=0)
+q17_a2.place(x=0, y=30)
+q17_a3.place(x=0, y=60)
+Question_18.place(x= 0, y= 120)
+q18_a1.place(x=0, y=120)
+q18_a2.place(x=0, y=150)
+q18_a3.place(x=0, y=180)
+Question_19.place(x= 0, y= 240)
+q19_a1.place(x=0, y=240)
+q19_a2.place(x=0, y=270)
+q19_a3.place(x=0, y=300)
+Question_20.place(x= 0, y= 360)
+q20_a1.place(x=0, y=360)
+q20_a2.place(x=0, y=390)
+q20_a3.place(x=0, y=420)
+
+Next_p5.place(x=0, y=480)
+
+###################################################################################################################
+
+#EVERYTHING TO DO WITH THE RESULTS PAGE
+
 window.mainloop()
